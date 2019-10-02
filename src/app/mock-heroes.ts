fbfbf6f93bfedb46a414a2dd1c6e65b1635c5356
derrick waweru <derrickwaweru38@gmail.com>
2019-10-02 15:01:17 +0300

    commited the rest of the files

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+// Displays a list of heroes. A sample hardcoded list
+export const HEROES: Hero[] = [
+    { id:11, name: 'Dr Who' },
+    { id: 12, name: 'Bombastic' },
+    { id: 13, name: 'Magenta' },
+    { id: 14, name: 'Dynamo' },
+    { id: 15, name: 'Elastic' },
+    { id:16, name: 'Blast' },
+    { id: 17, name: 'Magma' },
+    { id: 18, name: 'Tornado' },
+    { id: 19, name: 'Thunderbolt' },
+    { id: 20, name: 'Storm' }
+];
